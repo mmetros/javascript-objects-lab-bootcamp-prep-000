1bec6329324c985a5de3a8416597c1233180ca5a
mmetros@wesleyan.edu <mmetros@wesleyan.edu>
2018-08-02 19:13:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObject = object
+  var newObject = Object.assign({}, object)
 }
 
